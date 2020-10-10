680c718ffe7e43aabeb565e1bba75482d7fbb386
AokoMercyline <aokomercyline34@gmail.com>
2020-10-10 22:38:55 +0300

    created class for image description

--- mygallery/models.py
+++ mygallery/models.py
@@ -4,7 +4,13 @@ from cloudinary.models import CloudinaryField
 
 class Image(models.Model):
     caption = models.CharField(max_length=100)
+    title = models.CharField(max_length=200, default='')
+    description = models.TextField(default='')
+    location = models.TextField(max_length=30,null=False,blank=False, default='')
     image=CloudinaryField('image')
+    pub_date = models.DateTimeField(auto_now=True)
+    
+
     
     def __str__(self):
         return self.caption
