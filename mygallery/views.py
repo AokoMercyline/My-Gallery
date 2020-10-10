652603e19a25e926a84ee707248c93eb65d8686c
AokoMercyline <aokomercyline34@gmail.com>
2020-10-10 16:12:57 +0300

    created views

--- mygallery/views.py
+++ mygallery/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Image
 
 # Create your views here.
+def welcome(request):
+   title= 'Welcome  to my Gallerie'
+   images = Image.objects.all()
+   return render(request, 'mygallery/home.html', {'title' :title, 'images':images})
+
