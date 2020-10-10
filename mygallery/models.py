f47d9bcd411717526bba5acadee5188f65629156
AokoMercyline <aokomercyline34@gmail.com>
2020-10-10 16:12:11 +0300

    created class for uploading images with cloudinary

--- mygallery/models.py
+++ mygallery/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import cloudinary
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Image(models.Model):
+    caption = models.CharField(max_length=100)
+    image=CloudinaryField('image')
+    
+    def __str__(self):
+        return self.caption
+
+    class Meta:
+        verbose_name='Images'
+        
+        
