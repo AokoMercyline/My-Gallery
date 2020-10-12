f0eaae847480678d4e7de45f55a8385025e0e526
AokoMercyline <aokomercyline34@gmail.com>
2020-10-12 10:30:54 +0300

    created test for getting images by category

--- mygallery/tests.py
+++ mygallery/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
-
+from .models import Image,Category
 # Create your tests here.
+  
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        '''
+        Method to be run in every beginning of the test
+        '''
+        self.food= Category(category='food')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.food,Category))
+
+    def tearDown(self):
+        '''
+        Method to clear the test that has been done on category
+        '''
+        Category.objects.all().delete()
+
+    def test_save_method(self):
+        '''
+        Method to save category
+        
+        '''
+        self.food.save_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category)>0)
+
+    def test_delete_method(self):
+        '''
+        Method to delete the category
+        '''
+        self.food.delete_category('food')
+        category = Category.objects.all()
+        self.assertTrue(len(category)==0)
