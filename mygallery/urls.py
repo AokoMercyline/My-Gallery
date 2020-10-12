aba131a4b68cb4d01c4762695bf7c185c217a4d3
AokoMercyline <aokomercyline34@gmail.com>
2020-10-12 08:44:38 +0300

    importation

--- mygallery/urls.py
+++ mygallery/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from .views import welcome
+from django.conf.urls import url
+
+from . import views
+
 
 
 
 urlpatterns = [
-    path('',welcome, name='home')
+    path('',welcome, name='home'),
+    url(r'^search/', views.search_results, name='search_results')
     # path('dynamic/<slug:query>/' , dynamic_urls, name='dynamic')
 ]
