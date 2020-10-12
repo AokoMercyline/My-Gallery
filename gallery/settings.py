a5e82078d90712829b30ad3840a80c9103a87730
AokoMercyline <aokomercyline34@gmail.com>
2020-10-12 14:10:20 +0300

    fixed erro

--- gallery/settings.py
+++ gallery/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default=True)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', default=False, cast=bool)
+# DEBUG = os.environ.get('DEBUG', default=False, cast=bool)
 MODE=os.environ.get("MODE", default="dev")
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
 
